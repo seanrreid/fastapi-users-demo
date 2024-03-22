6cf58d2ba64e057aeb50482072859b34616d756e
Sean R Reid <sean@torchcodelab.com>
2024-03-22 19:16:47 -0400

    Updated local dev settings

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ class Settings:
     else:
         POSTGRES_USER: str = os.getenv("POSTGRES_USER")
         POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-        POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+        POSTGRES_HOST: str = os.getenv("POSTGRES_SERVER", "localhost")
         POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
         POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
         SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
